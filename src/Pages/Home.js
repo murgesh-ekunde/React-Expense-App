328166f80a2f4eefa9175684ad0ed16aa5e81949
Murgesh Ekunde <mekunde@gmail.com>
2022-11-26 23:51:25 +0530

    Added Components

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DefaultLayout from '../Components/DefaultLayout'
+
+function Home() {
+  return (
+    <DefaultLayout>
+        <h1>This is homepage </h1>
+    </DefaultLayout>
+  )
+}
+
+export default Home
